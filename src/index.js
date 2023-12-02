229dc9c3e893c62b615181cacb21710dec2e5a01
krishna sai gandi <39943509+gkrishnasai16@users.noreply.github.com>
2023-12-01 22:41:22 -0500

    frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Provider,  } from 'react-redux';
+import configureStore from './store/configureStore.js'
+import HomePage from './components/HomePage.js';
+import AppRouter from './routers/AppRouter.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = configureStore();
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(jsx,document.getElementById('app'));
+
